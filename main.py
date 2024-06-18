a3549ebc7580fff7ca7ce41cb79f4d4a038dfe32
Juan Luis Acebal <juanluisacebal@icloud.com>
2024-06-17 21:00:35 -0300

    otro commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import src.enunciado1_jacebal as e1
+import src.enunciado2_jacebal as e2
+
 
 def main():
     # URL del archivo CSV
@@ -6,6 +8,12 @@ def main():
     df = e1.read_csv(url)
     df = e1.clean_csv(df)
     df = e1.rename_col(df)
+    print("parte 2\n")
+    print(df.head(), "\n")
+    df = e2.breakdown_date(df)
+    df = e2.erase_month(df)
+    print(df.head())
+
 
 if __name__ == '__main__':
     main()
