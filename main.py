d658b8f3c572a63c4e3252868edbec5f3f5faa42
Juan Luis Acebal <juanluisacebal@icloud.com>
2024-06-17 23:28:40 -0300

    otro commit

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import src.enunciado1_jacebal as e1jla
 import src.enunciado2_jacebal as e2jla
 import src.enunciado3_jacebal as e3jla
+import src.enunciado4_jacebal as e4jla
 
 
 def main():
     # URL del archivo CSV
     url = 'data/nics-firearm-background-checks.csv'
     df = e1jla.read_csv(url)
+    df.hist()
     df = e1jla.clean_csv(df)
     df = e1jla.rename_col(df)
+    df.hist()
+    #e4jla.time_evolution(df)
     print("parte 2\n")
     print(df.head(), "\n")
     df = e2jla.breakdown_date(df)
     df = e2jla.erase_month(df)
-    print(df.head())
-
+    print(df[df['year']==2020].head())
+    df.hist()
     df = e3jla.groupby_state_and_year(df)
+    print(df[df['year']==2020].head())
     e3jla.print_biggest_handguns(df)
     e3jla.print_biggest_longguns(df)
+    e4jla.time_evolution(df)
+
 
 if __name__ == '__main__':
     main()
---
+++ src/enunciado4_jacebal.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import src.enunciado1_jacebal as e1jla
+
+
+def time_evolution(df: pd.DataFrame):
+    """
+    Creacion de grafico mostrando la evolucion de las licencias,
+    handguns y long guns en un periodo de tiempo.
+
+    Args:
+        df (DataFrame): DataFrame con columnas year, permit, handgun, y long_gun.
+    """
+    grouped_df = df.groupby('year').agg({
+        'permit': 'sum',
+        'handgun': 'sum',
+        'long_gun': 'sum'
+    }).reset_index()
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(grouped_df['year'], grouped_df['permit'], label='Permits', marker='o')
+    plt.plot(grouped_df['year'], grouped_df['handgun'], label='Handguns', marker='o')
+    plt.plot(grouped_df['year'], grouped_df['long_gun'], label='Long Guns', marker='o')
+
+    plt.title('Evolucion en el tiempo de permisos y armas de fuego')
+    plt.xlabel('Year')
+    plt.ylabel('Total')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    print("enunciado 4")
+    #url = './../data/nics-firearm-background-checks.csv'
+    #time_evolution(e1jla.read_csv(url))
